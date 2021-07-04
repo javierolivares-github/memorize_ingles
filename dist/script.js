1d3d4f862304308c4244bb0f764ea2bfecd01cbf
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-07-04 10:22:14 -0400

    av 04072021

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,16 @@
 // VARIABLES
 const tarjetas = document.querySelectorAll('.tarjeta');
 
+
+// FUNCIONES
 function voltearTarjeta(){
   // Comprobación en consola
   console.log('click!');
   console.log(this);
-
   // Agregando la clase voltear en forma toggle a tarjeta
   this.classList.toggle('voltear');
 }
+
 
 // EVENTOS
 tarjetas.forEach(tarjeta => tarjeta.addEventListener('click', voltearTarjeta));
