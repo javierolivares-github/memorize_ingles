d75ce9afacc99cb9974c7419a65d4b2a969b1bed
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-07-05 15:14:30 -0400

    av 05072021

--- dist/script.js
+++ dist/script.js
@@ -1,31 +1,32 @@
 // VARIABLES
+
 // Base de datos feak.
 const inglesBD = [
-  {"ingles": "be", "castellano": "ser o estar", "categoria": "verbo"},
-  {"ingles": "have", "castellano": "haber o tener", "categoria": "verbo"},
-  {"ingles": "there is", "castellano": "hay", "categoria": "verbo"},
-  {"ingles": "passport", "castellano": "pasaporte", "categoria": "sustantivo"},
-  {"ingles": "police", "castellano": "policía", "categoria": "sustantivo"},
-  {"ingles": "customs", "castellano": "aduanua", "categoria": "sustantivo"},
-  {"ingles": "good", "castellano": "bueno", "categoria": "adjetivo"},
-  {"ingles": "bad", "castellano": "malo", "categoria": "adjetivo"},
-  {"ingles": "ok", "castellano": "regular", "categoria": "adjetivo"},
-  {"ingles": "always", "castellano": "siempre", "categoria": "adverbio"},
-  {"ingles": "also", "castellano": "también", "categoria": "adverbio"},
-  {"ingles": "clear", "castellano": "claro", "categoria": "adverbio"},
+  {id: 1, ingles: "want", categoria: "verbo"},
+  {id: 2, ingles: "pasaporte", categoria: "sustantivo"},
+  {id: 3, ingles: "better", categoria: "adjetivo"},
+  {id: 4, ingles: "exact", categoria: "adverbio"},
+  {id: 5, ingles: "gracias", categoria: "cortesia"},
+  {id: 6, ingles: "briefcase", categoria: "sustantivo"},
+  {id: 7, ingles: "mejor", categoria: "adjetivo"},
+  {id: 8, ingles: "querer", categoria: "verbo"},
+  {id: 9, ingles: "exacto", categoria: "adverbio"},
+  {id: 10, ingles: "passport", categoria: "sustantivo"},
+  {id: 11, ingles: "maletín", categoria: "sustantivo"},
+  {id: 12, ingles: "thank you", categoria: "cortesia"}
 ];
-// Tarjetas 
-const tarjetas = document.querySelectorAll(".tarjeta");
-console.log(tarjetas)
-
-
 
 
 // FUNCIONES
-// Función crear tarjeta
-function crearTarjeta(){
+
+function voltearTarjeta(){
+  this.classList.toggle('voltear');
+}
+
+
+function crearUI(){
   inglesBD.forEach(item => {
-    // para cada elemento de mi matriz de objetos crea una ui.
+    // para cada elemento de mi matriz de objetos creale una ui.
     const tarjeta = document.createElement('div');
     tarjeta.classList = 'tarjeta voltear';
     tarjeta.innerHTML = `
@@ -33,34 +34,15 @@ function crearTarjeta(){
       <div class="cara cara--${item.categoria}">${item.ingles}</div>
     `;
 
-    // para cada ui muestra en consola.
-    console.log(tarjeta);
-
-
+    // Evento click y voltear
+    tarjeta.addEventListener('click', voltearTarjeta);
+    
     // para cada ui muestra en pantalla.
     document.getElementById('contenedorDeTarjetas').appendChild(tarjeta);
   });
 }
 
-// Función voltear tarjeta
-function voltearTarjeta(){
-  // Agrega la clase voltear en forma toggle a tarjeta.
-  this.classList.toggle('voltear');
-}
-// Función reproducir sonido.
-// function reproducirSonido(){}
-// Función detener sonido.
-// function detenerSonido() {}
-
-
-
-// EVENTOS
-// Evento al dar click voltea la tarjeta del memorice.
-// tarjetas.forEach(tarjeta => tarjeta.addEventListener('click', voltearTarjeta));
-// Evento al dar click se activa el sonido.
-// tarjetas.forEach(tarjeta => tarjeta.addEventListener('click', ))
-
 
 // INICIALIZACION DE FUNCIONES
-crearTarjeta();
+crearUI();
 
