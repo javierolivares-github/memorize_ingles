a4210fe644b4a7318b54502cf798092ece1760d7
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-07-05 11:22:21 -0400

    av 05072021

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,66 @@
 // VARIABLES
-const tarjetas = document.querySelectorAll('.tarjeta');
+// Base de datos feak.
+const inglesBD = [
+  {"ingles": "be", "castellano": "ser o estar", "categoria": "verbo"},
+  {"ingles": "have", "castellano": "haber o tener", "categoria": "verbo"},
+  {"ingles": "there is", "castellano": "hay", "categoria": "verbo"},
+  {"ingles": "passport", "castellano": "pasaporte", "categoria": "sustantivo"},
+  {"ingles": "police", "castellano": "policía", "categoria": "sustantivo"},
+  {"ingles": "customs", "castellano": "aduanua", "categoria": "sustantivo"},
+  {"ingles": "good", "castellano": "bueno", "categoria": "adjetivo"},
+  {"ingles": "bad", "castellano": "malo", "categoria": "adjetivo"},
+  {"ingles": "ok", "castellano": "regular", "categoria": "adjetivo"},
+  {"ingles": "always", "castellano": "siempre", "categoria": "adverbio"},
+  {"ingles": "also", "castellano": "también", "categoria": "adverbio"},
+  {"ingles": "clear", "castellano": "claro", "categoria": "adverbio"},
+];
+// Tarjetas 
+const tarjetas = document.querySelectorAll(".tarjeta");
+console.log(tarjetas)
+
+
 
 
 // FUNCIONES
+// Función crear tarjeta
+function crearTarjeta(){
+  inglesBD.forEach(item => {
+    // para cada elemento de mi matriz de objetos crea una ui.
+    const tarjeta = document.createElement('div');
+    tarjeta.classList = 'tarjeta voltear';
+    tarjeta.innerHTML = `
+      <div class="cara cara--frontal"></div>
+      <div class="cara cara--${item.categoria}">${item.ingles}</div>
+    `;
+
+    // para cada ui muestra en consola.
+    console.log(tarjeta);
+
+
+    // para cada ui muestra en pantalla.
+    document.getElementById('contenedorDeTarjetas').appendChild(tarjeta);
+  });
+}
+
+// Función voltear tarjeta
 function voltearTarjeta(){
-  // Comprobación en consola
-  console.log('click!');
-  console.log(this);
-  // Agregando la clase voltear en forma toggle a tarjeta
+  // Agrega la clase voltear en forma toggle a tarjeta.
   this.classList.toggle('voltear');
 }
+// Función reproducir sonido.
+// function reproducirSonido(){}
+// Función detener sonido.
+// function detenerSonido() {}
+
 
 
 // EVENTOS
-tarjetas.forEach(tarjeta => tarjeta.addEventListener('click', voltearTarjeta));
+// Evento al dar click voltea la tarjeta del memorice.
+// tarjetas.forEach(tarjeta => tarjeta.addEventListener('click', voltearTarjeta));
+// Evento al dar click se activa el sonido.
+// tarjetas.forEach(tarjeta => tarjeta.addEventListener('click', ))
+
+
+// INICIALIZACION DE FUNCIONES
+crearTarjeta();
 
